71c8bcc0819777cb0ff75f4f0dafa33e84def3b6
aswinzz <iit2016106@iiita.ac.in>
2017-06-16 01:17:46 +0530

    add change password facilty

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,40 @@
 var express = require('express');
 var router = express.Router();
+var User = require('../models/user');
+var bcrypt = require('bcryptjs');
+
+
+
+
 
 // Get Homepage
 router.get('/', ensureAuthenticated, function(req, res){
 	res.render('index');
+});
+
+router.post('/', function(req, res) {
+    
+      User.findOne({ email: req.body.email}, function(err, user) {
+        
+
+        bcrypt.genSalt(10, function(err, salt) {
+	    bcrypt.hash(req.body.password, salt, function(err, hash) {
+	        user.password = hash;
+	        
+
+        user.save(function(err) {
+          req.logIn(user, function(err) {
+          });
+	    });
+	});
+
+        
+        
+        });
+      });
+      res.redirect('/')
+    
+  
 });
 
 function ensureAuthenticated(req, res, next){
